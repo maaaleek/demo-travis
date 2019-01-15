9d2d70e3d8b6c60b9dd003090376b53d84c995d0
maaaleek <abdelh_m@etna-alternance.net>
2019-01-15 15:29:06 +0100

    First commit

---
+++ src/main/java/fr/ippon/training/demotravis/DemoTravisApplication.java
@@ -0,0 +1,14 @@
+package fr.ippon.training.demotravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTravisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTravisApplication.class, args);
+    }
+
+}
+
